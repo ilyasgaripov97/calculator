add76fdf4ade516b679c3bfadfd6b02f632bbc29
Ilyas Garipov <ilyasgaripov97@gmail.com>
2021-06-06 14:25:07 +0300

    Add function which fill display with pressed digit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let displayValue = 0;
+
 function add(a, b) {
   return a + b;
 }
@@ -24,3 +26,18 @@ const operationMapping = {
 function operate(a, b, operator) {
   return operationMapping[operator](a, b);
 }
+
+function populateDisplay() {
+  const displayOutput = document.querySelector('.display__output');
+  const digitKeys = document.querySelectorAll('.keys__digit');
+
+  digitKeys.forEach(key => {
+    key.addEventListener('click', e => {
+      const digit = e.target.value;
+      displayOutput.textContent = digit;
+      displayValue = digit;
+    })
+  })
+}
+
+populateDisplay();
